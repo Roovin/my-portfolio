0ffd5bbad4bdb3cfb4faf1f6a9d1a559dc89fab9
Roovin <roovinbansal@gmail.com>
2025-02-18 15:06:23 +0530

    Update

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link';
+import 'boxicons/css/boxicons.min.css';
 import BannerStyle from '../styles/banner.module.css';
+// import 'boxicons';
 
 export default function HeroBanner() {
   return (
@@ -8,14 +11,33 @@ export default function HeroBanner() {
     <div className="container">
         <div className={`${BannerStyle.wrapper} wrapper`}>
           <div className={`colOne ${BannerStyle.colOne}`}>
-            <h1>Hi, <br/>
+            <h1 className='font-bold'>Hi, <br/>
                 I'm <span className="text-[#4070f4]">Roovin</span><br/>
                 Web Designer</h1>
+            <div className="linkWrap">
+              <Link href="" className={`${BannerStyle.defaultBtn} font-bold`}>Contact Us</Link>
+            </div>
+            {/* <div className={`socialIconWrap ${BannerStyle.socialIconWrap}`}>
+              <ul>
+                <li>
+                  <div className={`${BannerStyle.iconWrap} iconWrap`}>
+                    <Link href={`/`} className='emptyLink'>.</Link>
+                    <i className='bx bxl-linkedin' ></i>
+                  </div>
+                </li>
+                <li>
+                  <div className="iconWrap">
+                  <i class='bx bxl-github' ></i>
+                  </div>
+                </li>
+              </ul>
+            </div> */}
           </div>
           <div className={`colOne ${BannerStyle.colOne}`}>
             <div className={`imageWrapper ${BannerStyle.imageWrapper}`}>
               <Image src={'/banner/self-pic.jpg'}  width={200} height={200} alt='Self Image' />
             </div>
+         
           </div>
         </div>
     </div>
